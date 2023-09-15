0d07e14ba5c4916005e47a31c57fd7091e6d0976
raheekhandigitalai <rahee.khan@digital.ai>
2023-09-15 16:24:46 -0400

    Upload File scenario

---
+++ fileupload_web.conf.js
@@ -0,0 +1,134 @@
+const properties = require('./properties');
+
+exports.config = {
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // continuos testing cloud:
+    hostname: 'uscloud.experitest.com',
+    protocol: 'https',
+    port: 443,
+    path: '/wd/hub',
+    
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the directory
+    // from which `wdio` was called.
+    //
+    specs: [
+        './test/specs/web-specs/web_miview_upload_file_test.js'
+    ],
+    //
+    // ============
+    // Capabilities
+    // ============
+    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+    // time. Depending on the number of capabilities, WebdriverIO launches several test
+    // sessions. Within your capabilities you can overwrite the spec and exclude options in
+    // order to group specific specs to a specific capability.
+    //
+    // First, you can define how many instances should be started at the same time. Let's
+    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+    // files and you set maxInstances to 10, all spec files will get tested at the same time
+    // and 30 processes will get spawned. The property handles how many capabilities
+    // from the same test should run tests.
+    //
+    maxInstances: 10,
+    //
+    //  For more Capabilitites go to:
+    //  https://docs.experitest.com/display/TE/Webdriver.io+Starter+Code
+
+    capabilities: [{
+        platformName: 'Windows',
+        'experitest:testName': "Upload File Test",
+        'experitest:accessKey': properties.accessKey,
+        browserName: 'chrome',
+        strictSSL: false
+    }
+    ],
+
+    acceptSslCerts: true,
+
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    // Level of logging verbosity: trace | debug | info | warn | error | silent
+    logLevel: 'info',
+    //
+    // Default timeout for all waitFor* commands.
+    //
+    waitforTimeout: 10000,
+    //
+    // Default timeout in milliseconds for request
+    // if browser driver or grid doesn't send response
+    connectionRetryTimeout: 120000,
+    //
+    // Default request retries count
+    connectionRetryCount: 3,
+    //
+    // Test runner services
+    // Services take over a specific job you don't want to take care of. They enhance
+    // your test setup with almost no effort. Unlike plugins, they don't add new
+    // commands. Instead, they hook themselves up into the test process.
+//    services: ['chromedriver'],
+    
+    // Framework you want to run your specs with.
+    // The following are supported: Mocha, Jasmine, and Cucumber
+    // see also: https://webdriver.io/docs/frameworks
+    //
+    // Make sure you have the wdio adapter package for the specific framework installed
+    // before running any tests.
+    framework: 'mocha',
+    //
+    // The number of times to retry the entire specfile when it fails as a whole
+    // specFileRetries: 1,
+    //
+    // Delay in seconds between the spec file retry attempts
+    // specFileRetriesDelay: 0,
+    //
+    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
+    // specFileRetriesDeferred: false,
+    //
+    // Test reporter for stdout.
+    // The only one supported by default is 'dot'
+    // see also: https://webdriver.io/docs/dot-reporter
+    reporters: ['spec'],
+
+
+    
+    //
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 180000
+    },
+
+    beforeTest: async function (test, context) {
+        await browser.setWindowSize(1920, 1080);
+    },
+
+    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+
+        const testStatus = passed ? "true" : "false";
+        const errorMessage = passed ? null : (error ? error.message : "Unknown error");
+        const stackTrace = passed ? null : (error ? error.stack : "No stack trace available");
+
+        // const seetestCommand = `seetest:client.setReportStatus("${testStatus}", "${errorMessage}", "${stackTrace}")`;
+         const seetestCommand = "seetest:client.setReportStatus";
+         const scriptArguments = [testStatus, errorMessage, stackTrace];
+
+         await browser.executeScript(seetestCommand, scriptArguments);
+
+        const reportUrl = await browser.capabilities.reportUrl;
+        console.log("Report URL: " + reportUrl);
+     }
+}
---
+++ test/specs/web-specs/web_miview_upload_file_test.js
@@ -0,0 +1,57 @@
+const { expect } = require('chai');
+const path = require('path');
+const filePath = path.join(__dirname, './dummyfile.csv');
+
+describe('LocalDeviceTest', () => {
+
+    it('quickStartDeviceWebDemo', async () => {
+        await browser.url('https://qa-web.miviewis.com')
+        const username = await $("//input[@placeholder='Username']");
+        await username.setValue('raheesuper@digital.ai');
+        const password = await $("//input[@placeholder='Password']");
+        await password.setValue('Demo123$');
+        const loginButton = await $("//button[@id='LoginButton']");
+        await loginButton.click();
+
+        const home = await $("//a[contains(text(), 'Demo Digital Home - 1')]");
+        await home.click();
+
+        const addDocument = await $("//button[@id='DocumentButton']");
+        await addDocument.click();
+
+        const remoteFilePath = await browser.uploadFile(filePath);
+
+        const chooseFile = await $("//input[@type='file']");
+        await chooseFile.waitForDisplayed({ timeout: 10000 });
+        await chooseFile.setValue(remoteFilePath);
+
+        await browser.pause(10000);
+
+        const typeDropdown = await $("(//*[contains(text(), 'Type') and @id='select-label']/following-sibling::div)[last()]");
+        await typeDropdown.click();
+
+        const typeItem = await $("//div[contains(text(), 'CAD - Water Line')]");
+        await typeItem.click();
+
+        const categoryDropdown = await $("(//*[contains(text(), 'Category') and @id='select-label']/following-sibling::div)[last()]");
+        await categoryDropdown.click();
+
+        const categoryItem = await $("//div[contains(text(), 'Rough')]");
+        await categoryItem.click();
+
+        const confirmButtonToSaveAttachment = await $("//div[contains(text(), 'CONFIRM')]");
+        await confirmButtonToSaveAttachment.click();
+
+        const uploadedAttachment = await $("//div[@data-testid='MiCard']");
+        await uploadedAttachment.waitForDisplayed({ timeout: 10000 });
+        expect(await uploadedAttachment.isDisplayed()).to.be.true;
+
+        const removeAttachmentButton = await $("(//div[@data-testid='MiCard']//*[@role='presentation'])[last()]");
+        await removeAttachmentButton.click();
+
+        const confirmButtonToRemoveAttachment = await $("//button[@id='ConfirmButton']");
+        await confirmButtonToRemoveAttachment.click();
+
+        await browser.pause(10000);
+    })
+})
